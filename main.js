89e42c4a5f5a4bd5e9d1c4a1042f57328ae73a4a
bromanovich99 <bromanovich99@gmail.com>
2021-07-19 18:26:53 +0300

    method and class

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+// Функция плюаризации слова. Сделать для всех случаев: 0, 2-4, 5-10, 11-19, 20-99, 100 и больше
+
+// Функция подсчета суммы в диапазоне целых чисел
+function summCalculate(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+//console.log(summCalculate(10,5))
+// Функция подсчета факториала до заданного элемента
+function factorialCalculate(number) {
+    let result = 1;
+    for(let counter = 1; counter<=number; counter++){
+        result = result * counter
+
+    }
+    return result;
+}
+console.log(factorialCalculate(3));
+5!=1*2*3*4*5
