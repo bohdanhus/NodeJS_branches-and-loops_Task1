c90adacd041dd86de4af20010aa3ae821a7a1ef5
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-07-20 00:34:33 +0300

    Update main.js
    Add plural function

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 // Функция плюаризации слова. Сделать для всех случаев: 0, 2-4, 5-10, 11-19, 20-99, 100 и больше
+//nom — Nominativ, именительный падеж;
+//gen — Genetiv, родительный падеж;
+//plu — Plural, множественное число.
+const titles = ['плюс', 'плюса', 'плюсов']
+
+console.log(Plural(12, titles));
+function Plural(num, titles) {  
+
+ let result;
+ let n = num % 100;
+ let i = num % 10; 
+   if (i => 11 && i <= 19) {
+ 	result = titles[0];
+ } else if (num == 0) {
+ 	result = ['Число должно быть больше единицы'];
+ } else if (i == 1) {
+ 	result = titles[0];
+ } else if (i => 2 && i <= 4) {
+ 	result = titles[1];
+ } else if (i => 5 && i <= 10) {
+ 	result = titles[2];
+ } else if (i => 11 && i <= 19) {
+ 	result = titles[0];
+ } else if (i => 2 && i <= 10) {
+ 	result = titles[2];
+ }
+ return result
+};
 
 // Функция подсчета суммы в диапазоне целых чисел
 function summCalculate(a, n) {
